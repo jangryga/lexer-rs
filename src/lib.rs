44372798f02d2baf5f2656676e839f9dbe1bc292
jangryga <jangryga@gmail.com>
2023-11-01 11:28:02 +0100

    split brain

--- src/lib.rs
+++ src/lib.rs
@@ -38,30 +38,65 @@ pub enum Token {
     WITH,
     YIELD,
 
+    IDENT,
+
     EOF,
 }
 
 pub struct Lexer {
     pub input: String,
-    pub cursor: u32,
+    pub position: u32,
+    pub read_position: u32,
     pub tokens: Vec<Token>,
+    pub character: u8
 }
 
 impl Lexer {
     pub fn new(input: &str) -> Lexer {
         Lexer {
-            cursor: 0,
+            character: 0,
+            position: 0,
+            read_position: 0,
             input: String::from(input),
             tokens: Vec::<Token>::new(),
         }
     }
 
-    pub fn handle_next_whitespace(&mut self) {}
+    pub fn handle_next_whitespace(&mut self) {
+    }
+
+    pub fn read_character(&mut self) -> Option<u8> {
+        if self.read_position as usize >= self.input.len() {
+            return None
+        }
+        self.position = self.read_position;
+        self.read_position += 1;
+        Some(self.input.chars().nth(self.read_position as usize).unwrap() as u8)
+    }
+
+    pub fn read_ident(&self) {
+        !unimplemented!()
+    }
 
     pub fn tokenize_character(&mut self) -> bool {
         self.handle_next_whitespace();
 
-        true
+        match self.read_character() {
+            Some(ch) => {
+                let mut token: Option<Token> = None;
+                match ch {
+                    b'a'..=b'z' | b'A'..=b'Z' | b'_' => {
+                        let indent = self.read_ident();
+                    }
+                    0 => token = Some(Token::EOF),
+                    _ => unreachable!("shouldn't reach this")
+                }
+                
+                self.tokens.push(token.unwrap());
+                true
+            }
+            None => false
+        }
     }
 
     pub fn tokenize(&mut self) {
